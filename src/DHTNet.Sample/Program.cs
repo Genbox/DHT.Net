2b471cc5de522c0c8bffc143d06497f44f006278
Ian Qvist <qvistian@gmail.com>
2016-11-13 01:02:35 +0100

    Moved folders

---
+++ src/DHTNet.Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DHTNet.Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
--- src/DHTNet.Sample/Properties/AssemblyInfo.cs
+++ src/DHTNet.Sample/Properties/AssemblyInfo.cs
@@ -5,24 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DHTNet")]
+[assembly: AssemblyProduct("DHTNet.Sample")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("1f088163-c7f5-483f-bd17-78cc5a409290")]
-[assembly: InternalsVisibleTo("DHTNet.Tests")]
-[assembly: AssemblyCopyright("Alan McGovern, Olivier Dufour 2009")]
-[assembly: AssemblyDescription("MonoTorrent DHT")]
-[assembly: AssemblyFileVersion("0.70")]
-[assembly: AssemblyInformationalVersion("0.70")]
-[assembly: AssemblyVersion("0.70")]
+[assembly: Guid("47e94a3c-b275-4732-8e49-f210e74d4323")]
